4b518fd460aed84cde076a06b7145a3f268ece5b
unknown <amanthakur.2003@gmail.com>
2024-04-04 21:35:16 +0530

    weatherapp7011

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "427bfbcdcc8f9617841517fd8f4f58c2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
